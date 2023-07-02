eda8aa9c83b857678b38ad5a63d467654ce4b4c6
riley anderson <riley.taylor.anderson@gmail.com>
2023-07-02 11:28:01 -0700

    adding hours, minutes, and functionality to clock

--- script.js
+++ script.js
@@ -3,11 +3,23 @@
     this.element = element;
     }
 
+    update() {
+        const parts = this.getTimeParts();
+        const minutesFormated = parts.minute.toString().padStart(2, "0"); 
+        const timeFormated = '${parts.hour}:${minutesFormatted}';
+        const amPm = parts.isAm ? "AM" : "PM";
+
+        this.element.querySelector(".clock-time")
+    }
+
 getTimeParts() {
     const now = new Date();
 
     return {
-        hour: now.getHours() % 12 || 12
+        hour: now.getHours() % 12 || 12,
+        minute: now.getMinutes(),
+        isAm: now.getHours() < 12
+        
     };
  }
 }
